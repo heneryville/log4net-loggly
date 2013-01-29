543d9a5d5833a7d9d8e66489b9e9e8f7380c92d0
Adam Hathcock <adam.hathcock@autodesk.com>
2013-01-29 09:34:28 +0000

    Updated Nuget managment.  Switched to JSON.NET

--- source/log4net-loggly/LogglyFormatter.cs
+++ source/log4net-loggly/LogglyFormatter.cs
@@ -2,51 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using ServiceStack.Text;
 using log4net.Core;
+using Newtonsoft.Json.Linq;
 
 namespace log4net.loggly
 {
-	public class LogglyFormatter : ILogglyFormatter
-	{
-		private Process _currentProcess;
+   public class LogglyFormatter : ILogglyFormatter
+   {
+      private Process _currentProcess;
 
-		public LogglyFormatter()
-		{
-			_currentProcess = Process.GetCurrentProcess();
-		}
+      public LogglyFormatter()
+      {
+         _currentProcess = Process.GetCurrentProcess();
+      }
 
-		public virtual void AppendAdditionalLoggingInformation(ILogglyAppenderConfig config, LoggingEvent loggingEvent)
-		{
-		}
+      public virtual void AppendAdditionalLoggingInformation(ILogglyAppenderConfig config, LoggingEvent loggingEvent)
+      {
+      }
 
-		public virtual string ToJson(LoggingEvent loggingEvent)
-		{
-			return PreParse(loggingEvent).ToJson();
-		}
+      public virtual string ToJson(LoggingEvent loggingEvent)
+      {
+         return PreParse(loggingEvent).ToString();
+      }
 
-		public virtual string ToJson(IEnumerable<LoggingEvent> loggingEvents)
-		{
-			return loggingEvents.Select(PreParse).ToJson();
-		}
+      public virtual string ToJson(IEnumerable<LoggingEvent> loggingEvents)
+      {
+         return new JArray(loggingEvents.Select(PreParse).ToArray()).ToString();
+      }
 
-		private object PreParse(LoggingEvent loggingEvent)
-		{
-			var exceptionString = loggingEvent.GetExceptionString();
-			if (string.IsNullOrWhiteSpace(exceptionString))
-			{
-				exceptionString = null; //ensure empty strings aren't included in the json output.
-			}
-			return new
-			       	{
-			       		level = loggingEvent.Level.DisplayName,
-			       		time = loggingEvent.TimeStamp.ToString("yyyyMMdd HHmmss.fff zzz"),
-						machine = Environment.MachineName,
-						process = _currentProcess.ProcessName,
-						thread = loggingEvent.ThreadName,
-						message = loggingEvent.MessageObject,
-						ex = exceptionString,
-			       	};
-		}
-	}
+      private JObject PreParse(LoggingEvent loggingEvent)
+      {
+         var exceptionString = loggingEvent.GetExceptionString();
+         if (string.IsNullOrWhiteSpace(exceptionString))
+         {
+            exceptionString = null; //ensure empty strings aren't included in the json output.
+         }
+         var jobject = new JObject();
+         jobject.Add("level", loggingEvent.Level.DisplayName);
+         jobject.Add("time", loggingEvent.TimeStamp.ToString("yyyyMMdd HHmmss.fff zzz"));
+         jobject.Add("machine", Environment.MachineName);
+         jobject.Add("process", _currentProcess.ProcessName);
+         jobject.Add("thread", loggingEvent.ThreadName);
+         jobject.Add("message", loggingEvent.MessageObject.ToString());
+         jobject.Add("ex", exceptionString);
+         return jobject;
+      }
+   }
 }
